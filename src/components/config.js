f5cb0c56e8594c422f3e5f2d3c13b5fb4516a449
Bea Malsky <beamalsky@datamade.us>
2020-06-05 16:19:15 -0500

    Move the map to Chicago

--- src/components/config.js
+++ src/components/config.js
@@ -9,13 +9,27 @@ var config = {
     footer: 'Source: source citations, etc.',
     chapters: [
         {
+            id: 'other-identifier',
+            title: 'Second Title',
+            image: 'test.jpg',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-87.62708, 41.88827],
+                zoom: 13.5,
+                pitch: 60,
+                bearing: -43.2
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
             id: 'slug-style-id',
             title: 'Display Title',
             image: 'test.jpg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 13.5,
+                center: [-87.62656, 41.88912],
+                zoom: 15,
                 pitch: 60,
                 bearing: 0
             },
@@ -31,20 +45,6 @@ var config = {
                 //     opacity: 0
                 // }
             ]
-        },
-        {
-            id: 'other-identifier',
-            title: 'Second Title',
-            image: 'test.jpg',
-            description: 'Copy these sections to add to your story.',
-            location: {
-                center: [-77.020636, 38.886900],
-                zoom: 13.5,
-                pitch: 60,
-                bearing: -43.2
-            },
-            onChapterEnter: [],
-            onChapterExit: []
         }
     ]
 };
